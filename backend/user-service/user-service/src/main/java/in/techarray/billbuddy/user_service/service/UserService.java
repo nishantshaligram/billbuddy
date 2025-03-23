00dc1e5c806688e536c70e5500f6b568fffa7b66
nishantshaligram <nishant.shaligram@gmail.com>
2025-03-24 03:21:25 +0530

    Add register user method to UserService

---
+++ backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/service/UserService.java
@@ -0,0 +1,9 @@
+package in.techarray.billbuddy.user_service.service;
+
+import in.techarray.billbuddy.user_service.dto.UserRequestDTO;
+import in.techarray.billbuddy.user_service.model.User;
+
+public interface UserService {
+
+    User registerUser( UserRequestDTO userRequestDTO);
+}
