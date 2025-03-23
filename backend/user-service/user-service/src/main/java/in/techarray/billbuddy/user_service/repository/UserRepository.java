72afdaad3df235d8d32591bc1b98f9442cedba49
nishantshaligram <nishant.shaligram@gmail.com>
2025-03-24 03:19:07 +0530

    Make findbyemail User return type Optional

--- backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/repository/UserRepository.java
+++ backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package in.techarray.billbuddy.user_service.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import in.techarray.billbuddy.user_service.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
