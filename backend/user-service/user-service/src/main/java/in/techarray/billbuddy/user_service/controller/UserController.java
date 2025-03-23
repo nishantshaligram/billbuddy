e08e8d55ddc54aa0ada52c8cb4415e713178598a
nishantshaligram <nishant.shaligram@gmail.com>
2025-03-24 03:30:28 +0530

    Add UserController for user registration endpoint

---
+++ backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/controller/UserController.java
@@ -0,0 +1,30 @@
+package in.techarray.billbuddy.user_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.techarray.billbuddy.user_service.dto.UserRequestDTO;
+import in.techarray.billbuddy.user_service.model.User;
+import in.techarray.billbuddy.user_service.service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping( "api/users" )
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<User> registerUser(@RequestBody UserRequestDTO userRequestDTO) {
+        User registeredUser = this.userService.registerUser(userRequestDTO);
+        return ResponseEntity.ok(registeredUser);
+    }
+    
+}
