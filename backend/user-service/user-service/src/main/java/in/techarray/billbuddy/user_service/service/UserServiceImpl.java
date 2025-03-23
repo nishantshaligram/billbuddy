4a2793fcefe1157652697860c8faa24ca6fea9a2
nishantshaligram <nishant.shaligram@gmail.com>
2025-03-24 03:24:53 +0530

    Implement User registration service with email uniqueness check

---
+++ backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package in.techarray.billbuddy.user_service.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import in.techarray.billbuddy.user_service.dto.UserRequestDTO;
+import in.techarray.billbuddy.user_service.model.User;
+import in.techarray.billbuddy.user_service.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+
+
+    @Override
+    public User registerUser(UserRequestDTO userRequestDTO) {
+        if( userRepository.findByEmail(userRequestDTO.getEmail()).isPresent()){
+            throw new RuntimeException("User with email already exists");
+        }
+
+        User user = new User();
+        user.setEmail( userRequestDTO.getEmail());
+        user.setPassword( passwordEncoder.encode( userRequestDTO.getPassword() ) );
+        return userRepository.save(user);
+    }
+
+}
