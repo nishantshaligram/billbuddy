1fc0244aeccc39823f15690befe26103fccca10b
nishantshaligram <nishant.shaligram@gmail.com>
2025-03-24 03:21:08 +0530

    Add User not found exception

---
+++ backend/user-service/user-service/src/main/java/in/techarray/billbuddy/user_service/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package in.techarray.billbuddy.user_service.exception;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
