b1733a4baddc58bd493e3463c7c2655328903e28
nishantshaligram <nishant.shaligram@gmail.com>
2025-05-04 00:04:42 +0530

    Initialize expense service

---
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/ExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package in.techarray.billbuddy.expense_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ backend/expense-service/src/test/java/in/techarray/billbuddy/expense_service/ExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techarray.billbuddy.expense_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
