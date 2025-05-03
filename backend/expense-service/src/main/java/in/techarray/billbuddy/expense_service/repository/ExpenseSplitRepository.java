2b18c8b38bf483777ea67ed797484f9b8ae85c34
nishantshaligram <nishant.shaligram@gmail.com>
2025-05-04 03:53:26 +0530

    Add ExpenseSplitRepository interface for managing ExpenseSplit entities

---
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/repository/ExpenseSplitRepository.java
@@ -0,0 +1,11 @@
+package in.techarray.billbuddy.expense_service.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.techarray.billbuddy.expense_service.model.ExpenseSplit;
+
+public interface ExpenseSplitRepository extends JpaRepository<ExpenseSplit, UUID> {
+
+}
