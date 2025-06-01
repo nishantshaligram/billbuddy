661aa62d640106a5a4420b7fb8689a24cd1b8482
nishantshaligram <nishant.shaligram@gmail.com>
2025-06-02 04:51:04 +0530

    Add authslice with login and register reducers

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+  user: null | { email: string, roles: Set<String>, createdAt: string, expiryAt: string };
+  token: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess: (state, action: PayloadAction<{ user: any; token: string }>) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
