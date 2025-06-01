3f4707750e6bccd38aac8df477eeaed45eff111b
nishantshaligram <nishant.shaligram@gmail.com>
2025-06-02 04:16:06 +0530

    Fix Cors Issue for Preflight Request from frontend

--- backend/user-service/src/main/java/in/techarray/billbuddy/user_service/security/SpringSecurity.java
+++ backend/user-service/src/main/java/in/techarray/billbuddy/user_service/security/SpringSecurity.java
@@ -1,11 +1,17 @@
 package in.techarray.billbuddy.user_service.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SpringSecurity {
@@ -13,13 +19,27 @@ public class SpringSecurity {
     @Order(1)
     @Bean
     SecurityFilterChain filteringCriteria(HttpSecurity http ) throws Exception{
-        http.cors( cors -> cors.disable() )
-        .csrf( csrf -> csrf.disable() )
-        .authorizeHttpRequests( authorize -> authorize.requestMatchers( "/auth/*").permitAll() )
-        // .authorizeHttpRequests( authorize -> authorize.requestMatchers( "/expense/*" ).authenticated() );
-        .authorizeHttpRequests( authorize -> authorize.anyRequest().permitAll() );
-
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(csrf -> csrf.disable())
+            // .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/*").permitAll())
+            // .authorizeHttpRequests( authorize -> authorize.requestMatchers( "/expense/*" ).authenticated() );
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:1000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/auth/**", config);
+        return source;
     }
 
     @Bean
