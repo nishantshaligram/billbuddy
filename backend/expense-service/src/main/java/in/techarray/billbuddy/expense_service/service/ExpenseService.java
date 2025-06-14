dc675d1a4917e743e057692beeb734ae8d1cd22c
nishantshaligram <nishant.shaligram@gmail.com>
2025-06-14 22:23:46 +0530

    Implement Split Strategy in Expense Service

--- backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/service/ExpenseService.java
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/service/ExpenseService.java
@@ -2,16 +2,24 @@ package in.techarray.billbuddy.expense_service.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import in.techarray.billbuddy.expense_service.dto.ExpenseRequestDto;
 import in.techarray.billbuddy.expense_service.model.Expense;
 import in.techarray.billbuddy.expense_service.model.ExpenseSplit;
 import in.techarray.billbuddy.expense_service.repository.ExpenseRepository;
 import in.techarray.billbuddy.expense_service.repository.ExpenseSplitRepository;
 import in.techarray.billbuddy.expense_service.strategy.EqualSplitStrategy;
+import in.techarray.billbuddy.expense_service.strategy.SplitStrategy;
+import in.techarray.billbuddy.expense_service.strategy.SplitStrategyFactory;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
 public class ExpenseService {
     private ExpenseRepository expenseRepository;
     private ExpenseSplitRepository expenseSplitRepository;
+    private SplitStrategyFactory strategyFactory;
 
     public ExpenseService(ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
@@ -20,23 +28,16 @@ public class ExpenseService {
     public Expense createExpenseWithSplits( ExpenseRequestDto expenseRequestDto ) {
         Expense expense = Expense.builder()
             .description(expenseRequestDto.getDescription())
-            .createdByUserId(expenseRequestDto.getCreatedByUserId())
             .totalAmount(expenseRequestDto.getTotalAmount())
             .date(expenseRequestDto.getDate())
-            .splitType(expenseRequestDto.getSplitType()).build();
+            .createdByUserId(expenseRequestDto.getCreatedByUserId())
+            .splitType(expenseRequestDto.getSplitType())
+            .build();
 
-            Expense savedExpense = expenseRepository.save(expense);
+        Expense savedExpense = expenseRepository.save(expense);
 
-        switch( expenseRequestDto.getSplitType() ){
-            case EQUAL:
-                EqualSplitStrategy equalSplitStrategy = new EqualSplitStrategy();
-                List<ExpenseSplit> expenseSplits = equalSplitStrategy.calculateSplits( savedExpense.getId(), expenseRequestDto);
-                expenseSplitRepository.saveAll( expenseSplits );
-                break;
-            default:
-                break;
-        }
-
+        SplitStrategy strategy = strategyFactory.getStrategy(expenseRequestDto.getSplitType());
+        List<ExpenseSplit> splits = strategy.calculateSplits(savedExpense.getId(), expenseRequestDto);
         return savedExpense;
     }
 
