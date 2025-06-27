ef6538cd827006897ebe1665fddc5f13cf8afe68
nishantshaligram <nishant.shaligram@gmail.com>
2025-06-28 02:02:02 +0530

    Refactor Expense and ExpenseSplit models to establish a one-to-many relationship and remove unused expenseId field

--- backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/model/Expense.java
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/model/Expense.java
@@ -1,7 +1,10 @@
 package in.techarray.billbuddy.expense_service.model;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.context.annotation.Lazy;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +12,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,4 +33,7 @@ public class Expense {
     @Enumerated(EnumType.STRING)
     private SplitType splitType;
     private LocalDate date;
+    @Lazy
+    @OneToMany
+    private List<ExpenseSplit> expenseSplits;
 }
--- backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/model/ExpenseSplit.java
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/model/ExpenseSplit.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,7 +22,6 @@ public class ExpenseSplit {
     @Id
     @GeneratedValue( strategy = GenerationType.UUID )
     private UUID id;
-    private UUID expenseId;
     private UUID userId;
     private Double amountOwed;
 }
