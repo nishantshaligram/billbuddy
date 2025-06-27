55ae1a417f27023c9ebc876e75461e19d9d727eb
nishantshaligram <nishant.shaligram@gmail.com>
2025-06-28 02:03:01 +0530

    Refactor ExpenseController to use ExpenseServiceImpl and add getExpense endpoint for retrieval by ID

--- backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/controller/ExpenseController.java
+++ backend/expense-service/src/main/java/in/techarray/billbuddy/expense_service/controller/ExpenseController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import in.techarray.billbuddy.expense_service.dto.ExpenseRequestDto;
 import in.techarray.billbuddy.expense_service.model.Expense;
 import in.techarray.billbuddy.expense_service.service.ExpenseService;
+import in.techarray.billbuddy.expense_service.service.ExpenseServiceImpl;
+
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,7 +24,7 @@ public class ExpenseController {
     private ExpenseService expenseService;
 
     
-    public ExpenseController(ExpenseService expenseService) {
+    public ExpenseController(ExpenseServiceImpl expenseService) {
         this.expenseService = expenseService;
     }
 
@@ -30,4 +35,9 @@ public class ExpenseController {
         return ResponseEntity.ok(expense);
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getExpense( @PathVariable UUID id ){
+        Expense expense = expenseService.getExpenseById( id );
+        return ResponseEntity.ok(expense);
+    }
 }
